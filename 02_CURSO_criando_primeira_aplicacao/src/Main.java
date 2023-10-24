afbfd1100d6879caf6284a98cc4fd27f60a05263
Marlon Prado <marlonprado04@gmail.com>
2023-10-24 17:25:00 -0300

    feat: add informations

--- 02_CURSO_criando_primeira_aplicacao/src/Main.java
+++ 02_CURSO_criando_primeira_aplicacao/src/Main.java
@@ -2,5 +2,16 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+
+        int ano = 2022;
+        String filme = "Top Gun: Maverick";
+        boolean incluidoNoPlano = true;
+        double notaFilme = 8.5;
+
+
+        System.out.println("Filme? " + filme);
+        System.out.println("Ano de lançamento? " + ano);
+        System.out.println("Esse filme está plano? " + incluidoNoPlano);
+        System.out.println("Nota do filme? " + notaFilme);
     }
 }
